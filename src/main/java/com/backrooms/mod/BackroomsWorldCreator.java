2ef876f59f8cafc015ecb0c3fb927caa683c47d8
misafurma-droid <misafurma@gmail.com>
2026-06-11 23:00:51 +0200

    Update BackroomsWorldCreator.java

--- src/main/java/com/backrooms/mod/BackroomsWorldCreator.java
+++ src/main/java/com/backrooms/mod/BackroomsWorldCreator.java
@@ -1,20 +1,12 @@
 package com.backrooms.mod;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.world.GeneratorType;
-import net.minecraft.server.integrated.IntegratedServer;
-import net.minecraft.util.registry.Registry;
-import net.minecraft.world.gen.chunk.ChunkGenerator;
 
+@Environment(EnvType.CLIENT)
 public class BackroomsWorldCreator {
-
-    /**
-     * Vytvoří nový singleplayer svět s Backrooms generací.
-     * Používá flat world s vlastním chunk generátorem.
-     */
     public static void createAndEnterWorld(MinecraftClient client) {
-        // Otevřeme screen pro tvorbu světa s předvyplněnými hodnotami
-        // Použijeme vlastní GeneratorType který použije BackroomsChunkGenerator
         client.setScreen(new BackroomsCreateWorldScreen(client.currentScreen));
     }
 }
